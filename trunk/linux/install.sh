b1768ec3faab8d824ff23008a7fe203b5b36bbd7
calidion@gmail.com <calidion@gmail.com@7f0dee4c-25e6-11df-a061-4f6072ac51cb>
2010-03-28 11:19:09 +0000

    added more

--- trunk/linux/install.sh
+++ trunk/linux/install.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/bash
+# 作者: 北漂IT民工 vim 自动安装版 version 0.1
+# 联系方式: calidion@gmail.com
+# google Code 地址: https://code.google.com/p/vimide/
 
 #vim directory preparation
 dir=~/.vim
@@ -34,6 +37,15 @@ getScript () {
   echo "$1 install finished."
 }
 
+getScriptFile () {
+  if [ ! -f "$1" ]; then
+    echo "Start downloading $1, please wait..."
+    wget http://www.vim.org/scripts/download_script.php?src_id=$2 -O $1
+  fi
+  cp -rf $1 $dir/plugin
+  echo "$1 install finished."
+}
+
 #install NERDTree
 # base: http://www.vim.org/scripts/script.php?script_id=1658
 getScript "NERDTree.zip" 11834
@@ -42,3 +54,24 @@ getScript "NERDTree.zip" 11834
 #install Tag List
 # base: http://www.vim.org/scripts/script.php?script_id=273
 getScript "taglist.zip" 7701
+echo "
+let Tlist_Ctags_Cmd='/usr/bin/ctags'
+" >> $vimrc
+
+#install miniBuffer
+# base: http://www.vim.org/scripts/script.php?script_id=159 
+getScriptFile "minibufexpl.vim" 3640
+echo "
+  let g:miniBufExplMapWindowNavVim = 1 
+  let g:miniBufExplMapWindowNavArrows = 1 
+  let g:miniBufExplMapCTabSwitchBufs = 1 
+  let g:miniBufExplModSelTarget = 1
+" >> $vimrc
+
+#install TaskList
+# base: http://www.vim.org/scripts/script.php?script_id=2607
+getScriptFile "tasklist.vim" 10388
+
+#install rails
+# base: http://www.vim.org/scripts/script.php?script_id=1567
+getScript "rails.zip" 12622
