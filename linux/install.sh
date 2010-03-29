a7ba0dc1e29919ea240703c26938c13e10c147b0
calidion@gmail.com <calidion@gmail.com@7f0dee4c-25e6-11df-a061-4f6072ac51cb>
2010-03-29 06:04:44 +0000

    added more plugins

--- linux/install.sh
+++ linux/install.sh
@@ -6,6 +6,14 @@
 # 安装方法: sh install.sh 或 chmod +x install.sh | ./install.sh
 # Install commands: sh install.sh or chmod +x install.sh | ./install.sh
 
+# 注意:
+# 必须要先执行下面的命令:
+# sudo apt-get install exuberant-ctags wget unzip
+
+# Caution:
+# must firstly run the following command:
+# sudo apt-get install exuberant-ctags wget unzip
+
 #global variables
 mydir=`pwd`
 tmpdir=/tmp/vimide
@@ -39,9 +47,11 @@ set noerrorbells
 set novisualbell
 set helplang=cn
 map <C-t> :NERDTree<cr>
+map <C-l> :TlistToggle<cr>
 map <C-s> :w<cr>
 vmap <C-c> \"+y
 " >> $vimrc
+
 if [ ! -d "$tmpdir" ]; then
   mkdir $tmpdir
 fi
@@ -70,6 +80,15 @@ getScript () {
     wget http://www.vim.org/scripts/download_script.php?src_id=$2 -O $1
   fi
   unzip -o $1 -d $dir
+  echo "$1 install finished."
+}
+
+getTarScript () {
+  if [ ! -f "$1" ]; then
+    echo "Start downloading $1, please wait..."
+    wget http://www.vim.org/scripts/download_script.php?src_id=$2 -O $1
+  fi
+  tar -xvf $1 -C $dir
   echo "$1 install finished."
 }
 
@@ -130,6 +149,60 @@ getFile "pythoncomplete.vim" 10872 /autoload
 # base: http://www.vim.org/scripts/script.php?script_id=152
 getScript "showmarks.zip" 2800
 
+#install vcscommand
+# base: http://vim.sourceforge.net/scripts/script.php?script_id=90
+getScript "vcscommand.zip" 12440
+
+#install cvim
+# base: http://vim.sourceforge.net/scripts/script.php?script_id=213
+#getScript "cvim.zip" 12311
+
+#install vimoutliner
+# base: http://vim.sourceforge.net/scripts/script.php?script_id=517
+getScript "vimoutliner.zip" 5768
+
+#install project
+# base: http://vim.sourceforge.net/scripts/script.php?script_id=69
+getTarScript "project-1.4.1.tar.gz" 6273
+
+#install a.vim
+# base: http://vim.sourceforge.net/scripts/script.php?script_id=31
+getScriptFile "a.vim" 7218
+
+#install php-doc
+# base: http://vim.sourceforge.net/scripts/script.php?script_id=1355
+getScriptFile "php-doc.vim" 4666
+echo "
+inoremap <C-P> <ESC>:call PhpDocSingle()<CR>i 
+nnoremap <C-P> :call PhpDocSingle()<CR> 
+vnoremap <C-P> :call PhpDocRange()<CR> 
+" >> $vimrc
+
+#install snipMate
+# base: http://vim.sourceforge.net/scripts/script.php?script_id=2540
+getScript "snipMate.zip" 11006
+
+#install omnicppcomplete
+# base: http://vim.sourceforge.net/scripts/script.php?script_id=1520
+getScript "omnicppcomplete.zip" 7722
+
+#install vimspell
+# base: http://vim.sourceforge.net/scripts/script.php?script_id=465
+#getScriptFile "vimspell.vim" 4580
+
+#install vim-autocomplpop
+# base: http://vim.sourceforge.net/scripts/script.php?script_id=1879
+getScript "vim-autocomplpop.zip" 11894
+
+#install perl-support
+# base: http://vim.sourceforge.net/scripts/script.php?script_id=556
+#getScript "perl-support.zip" 12539
+
+#install NERD_commenter
+# base: http://vim.sourceforge.net/scripts/script.php?script_id=1218
+getScript "NERD_commenter.zip" 10318
+
+#must be at the end of the file
 cd ..
 rm -rf $tmpdir
 cd $mydir
